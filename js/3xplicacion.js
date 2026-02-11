2f58f36333b04aab10ac2317f9929281e87f4d25
Saul <169074512+EndermanRD@users.noreply.github.com>
2026-02-11 14:34:11 -0400

    Add files via upload

---
+++ js/3xplicacion.js
@@ -0,0 +1,17 @@
+let btn=document.querySelector("#btn");
+let btn2=document.querySelector("#btn2");
+
+btn.addEventListener("click",function(){
+
+alert("Hola");
+
+
+})
+
+
+btn2.addEventListener("dblclick",function(){
+
+alert("Hola");
+
+
+})
